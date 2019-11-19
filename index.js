71edd9a5c30733590ad998852c0f0b085f62b722
Vitalie Braga <vibraga@microsoft.com>
2019-11-19 15:11:38 -0800

    Adding configuration

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
 const http = require("http");
 const url = require("url");
 const StringDecoder = require("string_decoder").StringDecoder;
+const config = require('./config');
 
 // The server should respond to all requests with a string
 var server = http.createServer((req, res) => {
@@ -76,8 +77,8 @@ var server = http.createServer((req, res) => {
 });
 
 // Start the server, and have it listen on port 3000
-server.listen(3000, () => {
-  console.log("The server is listening on port 3000 now.");
+server.listen(config.port, () => {
+  console.log(`The server is listening on port ${config.port} in ${config.envName} mode`);
 });
 
 // Define the handlers.
